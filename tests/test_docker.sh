0c67846ca62e363b2da731aa5b916824fd60ca60
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2016-10-07 18:01:28 -0300

    Update test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -3,14 +3,6 @@
 # it is meant to be run from the root directory of the repository, eg:
 # sh tests/test_docker.sh
 
-check_and_exit_if_error(){
-  docker-compose -f dev.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read -r code; do  
-      if [ "$code" != 0 ]; then    
-         exit -1
-      fi
-  done
-}
-
 # install test requirements
 pip install -r requirements.txt
 
@@ -23,9 +15,8 @@ cookiecutter ../../ --no-input --overwrite-if-exists
 cd project_name
 
 # run the project's tests
-docker-compose -f dev.yml run django python manage.py test
+#docker-compose -f dev.yml run django python manage.py test
 #check_and_exit_if_error
 
 # return non-zero status code if there are migrations that have not been created
-#docker-compose -f dev.yml run django python manage.py makemigrations --dry-run --check
-#check_and_exit_if_error
+docker-compose -f dev.yml run django python manage.py makemigrations --dry-run --check || { echo "there were changes, but the corresponding migration have not been created"; exit 1; }
